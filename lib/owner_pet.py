6dc946574939205ce2886b310e1b594fa9a2dcf1
Ruth <ruro122020@gmail.com>
2023-12-03 19:11:54 -0500

    complete lab

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,62 @@
+
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = list()
+
+    def __init__(self, name, pet_type, owner=None):
+        self.owner = owner
+        self.name = name
+        self.pet_type = pet_type
+        Pet.all.append(self)
+        
+    @property
+    def pet_type(self):
+        return self._pet_type
+    
+    @pet_type.setter
+    def pet_type(self, pet_type):
+        if pet_type in self.PET_TYPES:
+            self._pet_type = pet_type
+        else:
+            raise Exception 
+    
+    @property
+    def owner(self):
+        return self._owner
+    
+    @owner.setter
+    def owner(self, owner):
+        if not (isinstance(owner, Owner) or not owner):
+            raise Exception("Object is not of type Owner")
+        self._owner = owner
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+       
+
+    def pets(self):
+        pets = []
+        for pet in Pet.all:
+            if pet.owner == self:
+                pets.append(pet)
+        return pets
+        
+    def add_pet(self, pet):
+        if not (isinstance(pet, Pet)) or not pet:
+            raise Exception("Object is not of type Pet")
+        else:
+            pet.owner = self
+            
+    
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda pet: pet.name)
+    
+        
+# owner = Owner("John")
+# pet1 = Pet("Fido", "dog", owner)
+# pet2 = Pet("Clifford", "dog", owner)
+# pet3 = Pet("Whiskers", "cat", owner)
+# pet4 = Pet("Jerry", "reptile", owner)
+
+# print(owner.get_sorted_pets())
